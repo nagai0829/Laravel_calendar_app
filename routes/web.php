97a9f9847b40868931d8f05f234a617860f1ca67
nagai0829 <ke.seiya0829@gmail.com>
2021-03-05 15:15:24 +0900

    firstcommit

---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CalendarController;
+use App\Http\Controllers\HomeController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Auth::routes();
+Route::get('/', [CalendarController::class, 'show']);
+Route::get('/home', [HomeController::class, 'index']);
